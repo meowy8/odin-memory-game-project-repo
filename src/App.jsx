b360819bfc14495d697197f131b7e8a2cff3b651
meowy8 <mhoey8@hotmail.com>
2023-12-24 15:15:34 +0000

    setup for card and App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './Components/Card'
 
 function App() {
+  const [ currentScore, setCurrentScore ] = useState(0)
+  const [ bestScore, setBestScore ] = useState(0)
 
   return (
+    <div>
+      {images.forEach(image => {
+        return <Card imgSrc={image.src}/>
+      })}
+    </div>
   )
 }
 
